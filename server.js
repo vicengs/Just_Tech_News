2b9afc60ddf8d041bb23782b3cd7a0501079adc0
Vicente Garcia <vicen1@hotmail.com>
2022-04-26 19:19:26 -0500

    Add count votes method

--- server.js
+++ server.js
@@ -8,6 +8,6 @@ app.use(express.urlencoded({ extended: false }));
 // turn on routes
 app.use(routes);
 // turn on connection to db and server
-sequelize.sync({ force: true }).then(() => {
+sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log('Now listening'));
 });
