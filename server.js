1cfcef558f4291d02809ad2f1dce7be9cc86674d
Vicente Garcia <vicen1@hotmail.com>
2022-04-29 18:29:11 -0500

    Setup homepage

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
+const path = require('path');
 const express = require('express');
 const routes = require('./routes');
 const sequelize = require('./config/connection');
 const app = express();
 const PORT = process.env.PORT || 3001;
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 // turn on routes
