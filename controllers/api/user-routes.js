043dda9645ede0329fb2fb11bcb85a4f8971e7e8
Vicente Garcia <vicen1@hotmail.com>
2022-05-04 15:31:19 -0500

    Add logout functionality

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -62,15 +62,20 @@ router.post('/', (req, res) => {
         email: req.body.email,
         password: req.body.password
     })
-    .then(dbUserData => res.json(dbUserData))
+    .then(dbUserData => {
+        req.session.save(() => {
+            req.session.user_id = dbUserData.id;
+            req.session.username = dbUserData.username;
+            req.session.loggedIn = true;
+            res.json(dbUserData);
+        });
+    })
     .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
+    console.log(err);
+    res.status(500).json(err);
     });
 });
-
 router.post('/login', (req, res) => {
-    // expects {email: 'lernantino@gmail.com', password: 'password1234'}
     User.findOne({
         where: { email: req.body.email }
     })
@@ -78,15 +83,30 @@ router.post('/login', (req, res) => {
         if (!dbUserData) {
             res.status(400).json({ message: 'No user with that email address!' });
             return;
-        };
-        // Verify user
+        }
         const validPassword = dbUserData.checkPassword(req.body.password);
         if (!validPassword) {
             res.status(400).json({ message: 'Incorrect password!' });
             return;
-        };
-        res.json({ user: dbUserData, message: 'You are now logged in!' });
-    });  
+        }
+        req.session.save(() => {
+            // declare session variables
+            req.session.user_id = dbUserData.id;
+            req.session.username = dbUserData.username;
+            req.session.loggedIn = true;
+            res.json({ user: dbUserData, message: 'You are now logged in!' });
+        });
+    });
+});
+router.post('/logout', (req, res) => {
+    if (req.session.loggedIn) {
+        req.session.destroy(() => {
+                res.status(204).end();
+        });
+    }
+    else {
+        res.status(404).end();
+    }
 });
 // PUT /api/users/1
 router.put('/:id', (req, res) => {
